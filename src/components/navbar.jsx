6b48a5877e95bfc7f7d8cb29bffdc4aad63c429d
Gary Wallen <garycwallen@gmail.com>
2024-03-25 22:00:54 -0400

    Setup active links in the Navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,6 +3,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { useState } from 'react';
+import NavLink from './navLink';
 
 const links = [
   { url: '/', title: 'Home' },
@@ -15,17 +16,15 @@ export const Navbar = () => {
   const [open, setOpen] = useState(false);
 
   return (
-    <div className='h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48'>
+    <div className='h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 text-xl'>
       {/* Links */}
       <div className='hidden md:flex gap-4 w-1/3'>
         {links.map((link) => (
-          <Link href={link.url} key={link.url}>
-            {link.title}
-          </Link>
+          <NavLink link={link} key={link.url} />
         ))}
       </div>
       {/* LOGO */}
-      <div className='md:hidden lg:flex w-1/3 justify-center'>
+      <div className='md:hidden lg:flex xl:w-1/3 xl:justify-center'>
         <Link
           href='/'
           className='text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center'
