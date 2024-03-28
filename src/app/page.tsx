3ca2917cf270772d24b0b9c1a7f3264fc5180154
Gary Wallen <garycwallen@gmail.com>
2024-03-27 21:31:53 -0400

    Added hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,30 @@
+import Image from 'next/image';
+
 const Homepage = () => {
-  return <div>Hello World</div>;
+  return (
+    <div className='h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48'>
+      {/* Image Container */}
+      <div className='h-1/2 lg:h-full lg:w-1/2 relative'>
+        <Image src='/hero.png' alt='' fill className='object-contain' />
+      </div>
+      {/* Text Container */}
+      <div className='h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center'>
+        {/* Title */}
+        <h1 className='text-4xl md:text-6xl font-bold'>BLAH BLAH</h1>
+        {/* Description */}
+        <p className='md:text-xl'>BLAH BLAH BLAH</p>
+        {/* Buttons */}
+        <div className='w-full flex gap-4'>
+          <button className='p-4 rounded-lg ring-1 ring-black bg-black text-white'>
+            View My Work
+          </button>
+          <button className='p-4 rounded-lg ring-1 ring-black'>
+            Contact Me
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Homepage;
