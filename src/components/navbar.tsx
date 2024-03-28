0c190fc2409edc56dddf48d9b0694517e09f49c7
Gary Wallen <garycwallen@gmail.com>
2024-03-27 21:55:40 -0400

    Added framer motion to responsive menu

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -22,10 +22,9 @@ export const Navbar = () => {
     },
     opened: {
       rotate: 45,
-      backgroundColor: 'rgb(255, 255, 255)',
+      backgroundColor: 'rgb(255,255,255)',
     },
   };
-
   const centerVariants = {
     closed: {
       opacity: 1,
@@ -41,7 +40,7 @@ export const Navbar = () => {
     },
     opened: {
       rotate: -45,
-      backgroundColor: 'rgb(255, 255, 255)',
+      backgroundColor: 'rgb(255,255,255)',
     },
   };
 
@@ -51,15 +50,30 @@ export const Navbar = () => {
     },
     opened: {
       x: 0,
+      transition: {
+        when: 'beforeChildren',
+        staggerChildren: 0.2,
+      },
+    },
+  };
+
+  const listItemVariants = {
+    closed: {
+      x: -10,
+      opacity: 0,
+    },
+    opened: {
+      x: 0,
+      opacity: 1,
     },
   };
 
   return (
     <div className='h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 text-xl'>
-      {/* Links */}
+      {/* LINKS */}
       <div className='hidden md:flex gap-4 w-1/3'>
         {links.map((link) => (
-          <NavLink link={link} key={link.url} />
+          <NavLink link={link} key={link.title} />
         ))}
       </div>
       {/* LOGO */}
@@ -68,23 +82,24 @@ export const Navbar = () => {
           href='/'
           className='text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center'
         >
-          <span className='text-white mr-1'>nomadDev</span>
+          <span className='text-white mr-1'>Lama</span>
           <span className='w-12 h-8 rounded bg-white text-black flex items-center justify-center'>
-            .io
+            .dev
           </span>
         </Link>
       </div>
-      {/* Social Media */}
+      {/* SOCIAL */}
       <div className='hidden md:flex gap-4 w-1/3'>
         <Link href='https://github.com/garycwallen'>
           <Image src='/github.png' alt='' width={24} height={24} />
         </Link>
       </div>
-      {/* Responsive Menu */}
+      {/* RESPONSIVE MENU */}
       <div className='md:hidden'>
+        {/* MENU BUTTON */}
         <button
           className='w-10 h-8 flex flex-col justify-between z-50 relative'
-          onClick={() => setOpen(!open)}
+          onClick={() => setOpen((prev) => !prev)}
         >
           <motion.div
             variants={topVariants}
@@ -102,22 +117,26 @@ export const Navbar = () => {
             className='w-10 h-1 bg-black rounded origin-left'
           ></motion.div>
         </button>
+        {/* MENU LIST */}
+        {open && (
+          <motion.div
+            variants={listVariants}
+            initial='closed'
+            animate='opened'
+            className='absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl z-40'
+          >
+            {links.map((link) => (
+              <motion.div
+                variants={listItemVariants}
+                className=''
+                key={link.title}
+              >
+                <Link href={link.url}>{link.title}</Link>
+              </motion.div>
+            ))}
+          </motion.div>
+        )}
       </div>
-      {/* MENU LIST */}
-      {open && (
-        <motion.div
-          variants={listVariants}
-          initial='closed'
-          animate='opened'
-          className='absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl'
-        >
-          {links.map((link) => (
-            <Link href={link.url} key={link.url}>
-              {link.title}
-            </Link>
-          ))}
-        </motion.div>
-      )}
     </div>
   );
 };
