53a7307dd9253197cd576f041201a7512c2e55fc
Gary Wallen <garycwallen@gmail.com>
2024-03-27 21:44:49 -0400

    Added Framer Motion and apply motion to hamburger menu

--- README.md
+++ README.md
@@ -1,20 +1,12 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+# Animated Portfolio with nextJS + Framer Animation
 
-## Getting Started
+## Scripts
 
-First, run the development server:
+- `npm run dev`: Start local server
 
-`npm run dev`
-
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
-
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
+## Resources
+- [Eslint](https://eslint.org/)
+- [Framer Motion](https://www.framer.com/)
+- [nextJS](https://nextjs.org/)
+- [TailwindCSS](https://tailwindcss.com/)
+- [TypeScript](https://www.typescriptlang.org/)
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { useState } from 'react';
 import NavLink from './navLink';
+import { motion } from 'framer-motion';
 
 const links = [
   { url: '/', title: 'Home' },
@@ -14,6 +15,44 @@ const links = [
 
 export const Navbar = () => {
   const [open, setOpen] = useState(false);
+
+  const topVariants = {
+    closed: {
+      rotate: 0,
+    },
+    opened: {
+      rotate: 45,
+      backgroundColor: 'rgb(255, 255, 255)',
+    },
+  };
+
+  const centerVariants = {
+    closed: {
+      opacity: 1,
+    },
+    opened: {
+      opacity: 0,
+    },
+  };
+
+  const bottomVariants = {
+    closed: {
+      rotate: 0,
+    },
+    opened: {
+      rotate: -45,
+      backgroundColor: 'rgb(255, 255, 255)',
+    },
+  };
+
+  const listVariants = {
+    closed: {
+      x: '100vw',
+    },
+    opened: {
+      x: 0,
+    },
+  };
 
   return (
     <div className='h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 text-xl'>
@@ -47,20 +86,37 @@ export const Navbar = () => {
           className='w-10 h-8 flex flex-col justify-between z-50 relative'
           onClick={() => setOpen(!open)}
         >
-          <div className='w-10 h-1 bg-black rounded'></div>
-          <div className='w-10 h-1 bg-black rounded'></div>
-          <div className='w-10 h-1 bg-black rounded'></div>
+          <motion.div
+            variants={topVariants}
+            animate={open ? 'opened' : 'closed'}
+            className='w-10 h-1 bg-black rounded origin-left'
+          ></motion.div>
+          <motion.div
+            variants={centerVariants}
+            animate={open ? 'opened' : 'closed'}
+            className='w-10 h-1 bg-black rounded'
+          ></motion.div>
+          <motion.div
+            variants={bottomVariants}
+            animate={open ? 'opened' : 'closed'}
+            className='w-10 h-1 bg-black rounded origin-left'
+          ></motion.div>
         </button>
       </div>
       {/* MENU LIST */}
       {open && (
-        <div className='absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl'>
+        <motion.div
+          variants={listVariants}
+          initial='closed'
+          animate='opened'
+          className='absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl'
+        >
           {links.map((link) => (
             <Link href={link.url} key={link.url}>
               {link.title}
             </Link>
           ))}
-        </div>
+        </motion.div>
       )}
     </div>
   );
